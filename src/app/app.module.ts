4cc39deea280dba39b557492f9f80fcebf4530d3
pradeepradyumna <pradeepradyumna@gmail.com>
2020-05-04 19:21:36 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-
-@NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
-})
-export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';  
+import { HttpClientModule } from '@angular/common/http';  
+import { AppRoutingModule } from './app-routing.module';  
+import { ReactiveFormsModule } from "@angular/forms";  
+  
+import { AppComponent } from './app.component';  
+import { ListEmpComponent } from './list-emp/list-emp.component';  
+import { AddEmpComponent } from './add-emp/add-emp.component';  
+import { EmployeeService } from './service/employee.service';  
+  
+@NgModule({  
+  declarations: [  
+    AppComponent,  
+    ListEmpComponent,  
+    AddEmpComponent  
+  ],  
+  imports: [  
+    BrowserModule,  
+    HttpClientModule,  
+    AppRoutingModule,  
+    ReactiveFormsModule  
+  ],  
+  providers: [EmployeeService],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }  
+  
+Model  
+export class Employee {  
+    id?: number;  
+    employee_name?: string;  
+    employee_salary?: number;  
+    employee_age?: number;  
+}  
